beb91cc9ec9ca0f05b5e4ed3365ff702f0f4eca5
HotBloodMan <18303011272@163.com>
2019-06-09 10:47:26 +0800

    transation

--- greendao/src/main/java/com/jackli/greendao/MainActivity.java
+++ greendao/src/main/java/com/jackli/greendao/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import com.jackli.greendao.test.CreditCardDao;
 import com.jackli.greendao.test.StudentDao;
 
+import org.greenrobot.greendao.query.QueryBuilder;
+
 import java.util.List;
 
 import butterknife.BindView;
@@ -43,11 +45,13 @@ public class MainActivity extends AppCompatActivity {
         switch (v.getId()){
             case R.id.btn_insert:
 //                historyDataDao.insert(new HistoryData("A","data1"));
-                Student student = new Student();
-                student.setStudentNo(2);
-                student.setAge(11);
-                student.setName("lisi2");
-                studentDao.insert(student);
+                for(int i=5;i<50;i++){
+                    Student student = new Student();
+                    student.setStudentNo(i);
+                    student.setAge(i*3);
+                    student.setName("lisi"+i);
+                    studentDao.insert(student);
+                }
 //                for(int i=0;i<10;i++){
                     CreditCard creditCard = new CreditCard();
                     creditCard.setCardNum(11+"");
@@ -66,9 +70,20 @@ public class MainActivity extends AppCompatActivity {
 //                for(int i=0;i<students.size();i++){
 //                    Log.d("TAG","doClick load="+students.get(i).toString());
 //                }
-                List<Student> list = studentDao.queryBuilder().where(StudentDao.Properties.StudentNo.eq("2")).list();
-                Log.d("TAG"," list= "+list.size());
+//                List<Student> list = studentDao.queryBuilder().where(StudentDao.Properties.StudentNo.eq("2")).list();
+//                Log.d("TAG"," list= "+list.size());
 
+                //between
+//                List<Student> list = studentDao.queryBuilder().where(StudentDao.Properties.StudentNo.between(10, 19)).list();
+//                Log.d("TAG"," list= "+list.size());
+
+                // 大于gt(12)  orderDesc 降序
+                List<Student> list = studentDao.queryBuilder().where(StudentDao.Properties
+                        .StudentNo.gt(12)).orderDesc(StudentDao.Properties.Age).list();
+                Log.d("TAG"," list= "+list.size());
+                for(int i=0;i<list.size();i++){
+                    Log.d("TAG"," student= "+list.get(i).toString());
+                }
 
 //                List<HistoryData> historyData = historyDataDao.loadAll();
 //                if(historyData.size()<=0){
